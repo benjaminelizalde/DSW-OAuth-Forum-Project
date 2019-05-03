46195f92bb3b0c71fd0d429b46b9f8da358ac09a
mesa <mehufarm@gmail.com>
2019-05-03 11:13:33 -0700

    yeet

---
+++ env.bat
@@ -0,0 +1,4 @@
+set MONGO_USERNAME=mehufarm
+set MONGO_PASSWORD=tjnYt8MPU8JTaRx5
+set MONGO_HOST=mongodb+srv://mehufarm:tjnYt8MPU8JTaRx5@forum-7kimj.mongodb.net/test?retryWrites=true
+set MONGO_DBNAME=Forum-Posts
--- webapp.py
+++ webapp.py
@@ -3,10 +3,14 @@ from flask import Flask, redirect, url_for, session, request, jsonify, Markup
 from flask_oauthlib.client import OAuth
 from flask import render_template
 from time import localtime, strftime
+from bson.objectid import ObjectId
 
 import pprint
 import os
 import json
+import pymongo
+import dns
+import sys
 
 app = Flask(__name__)
 
@@ -14,7 +18,16 @@ app.debug = True #Change this to False for production
 app.secret_key = os.environ['SECRET_KEY'] #used to sign session cookies
 oauth = OAuth(app)
 
-#Set up GitHub as OAuth provider
+url = 'mongodb+srv://{}:{}@{}/{}'.format(
+    os.environ["MONGO_USERNAME"],
+    os.environ["MONGO_PASSWORD"],
+    os.environ["MONGO_HOST"],
+    os.environ["MONGO_DBNAME"]
+)
+client = pymongo.MongoClient(os.environ["MONGO_HOST"])
+db = client[os.environ["MONGO_DBNAME"]]
+collection = db['Posts']
+    #Set up GitHub as OAuth provider
 github = oauth.remote_app(
     'github',
     consumer_key=os.environ['GITHUB_CLIENT_ID'], #your web app's "username" for github's OAuth
@@ -33,23 +46,6 @@ pdata2="static/posts2.json"
 #TODO: Create the file on Heroku using os.system.  Ex) os.system("echo '[]'>"+myFile) puts '[]' into your file
 #os.system("echo '[]'>"+pdata)
 
-def Post_Html1():
-    with open(pdata1,"r") as postfile:
-        data=json.load(postfile)
-        data.reverse()
-        mass =""
-        for com in data:
-            mass += com["user"] + ": "+com["message"] + "<br>"
-    return mass
-
-def Post_Html2():
-    with open(pdata2,"r") as postfile:
-        data=json.load(postfile)
-        data.reverse()
-        mass =""
-        for com in data:
-            mass += com["user"] + ": "+com["message"] + "<br>"
-    return mass
 
 @app.context_processor
 def inject_logged_in():
@@ -58,59 +54,55 @@ def inject_logged_in():
 @app.route('/')
 def Forum1():
     print(strftime("%a, %d %b %Y %H:%M:%S", localtime()))
-    with open(pdata1,"r") as postfile:
-        data=json.load(postfile)
-        data.reverse()
-        mass =""
-        for com in data:
-            mass += com["user"] + ": "+com["message"] + "<br>"
-        return render_template('Forum1.html', past_posts1=Markup(mass))
+    return render_template('Forum1.html')
 
 @app.route('/Forum2')
 def Forum2():
-    redirect='Forum2'
-    print(strftime("%a, %d %b %Y %H:%M:%S", localtime()))
-    with open(pdata2,"r") as postfile:
-        data=json.load(postfile)
-        data.reverse()
-        mass =""
-        for com in data:
-            mass += com["user"] + ": "+com["message"] + "<br>"
-        return render_template('Forum2.html', past_posts2=Markup(mass))
+    return render_template('Forum2.html')
 
 @app.route('/posted1', methods=['POST'])
 def post1():
     msg = request.form['message']
+    print(msg)
     usr = session['user_data']['login'];
+    print(usr)
     newpost = {}
-    if usr == "DaZenMesa" or usr == "benjaminelizalde":
-        newpost["user"] = "<font color=#0008ff> <b>" +'[ADMIN]'+" "+ usr + "</b></font>"
-    else:
-        newpost["user"] = "<font color=#000000> <b>" + usr + "</b></font>"
-    newpost["message"] = "<b>" + msg + "</b>" + ' :' + str(strftime( "%H:%M:%S", localtime()))
+    newpost["user"] = usr
+    newpost['msg']=msg
+    print(newpost)
+    collection.insert_one(newpost)
 
-    #alldata += newpost
+    post_data = collection.find({})
+
+    print(post_data)
+
+#    post_data["user"] = usr
+#    post_data["message"] = msg +': ' + str(strftime( "%H:%M:%S", localtime()))
+    #for x in post_data:
+    #   print(x)
+    #   username=x['user']
+    #   message=x['msg']
+    #   allposts=allposts +thedata
+    #print (allposts)
+    #post_data["user"] = username
+    #post_data["message"] = message +': ' + str(strftime( "%H:%M:%S", localtime()))
+
+    #print(username)
+    #print(message)
     #os.run( json(alldata) > file )
-    with open(pdata1,'r') as oldpost:
-        data=json.load(oldpost)
-        data.append(newpost)
        # oldpost.seek(0)
         #oldpost.truncate()
         #data.append(newpost)
 
-    with open(pdata1,'w') as oldpost:
-        json.dump(data,oldpost)
-
     #return render_template('home.html' )
-    print(request.form)
     redirect= ""
 
     if "Forum1" in request.form:
         redirect= "Forum1"
-        return render_template('%s.html' % redirect, past_posts1=Markup(Post_Html1()))
+        return render_template('%s.html' % redirect, past_posts1=post_data)
     if "Forum2" in request.form:
         redirect= "Forum2"
-        return render_template('%s.html' % redirect, past_posts2=Markup(Post_Html2()))
+        return render_template('%s.html' % redirect)
     print(redirect)
 
     #return redirect(url_for('.' + redirect))
@@ -165,7 +157,7 @@ def post2():
 #redirect to GitHub's OAuth page and confirm callback URL
 @app.route('/login')
 def login():
-    return github.authorize(callback=url_for('authorized', _external=True, _scheme='https')) #callback URL must match the pre-configured callback URL
+    return github.authorize(callback=url_for('authorized', _external=True, _scheme='http')) #callback URL must match the pre-configured callback URL
 
 @app.route('/logout')
 def logout():
